550d687df665ee53e71fd8eb5c699e2f56a81db9
juancruzgalloloreti <juancruzgalloloreti@gmail.com>
2026-04-08 20:14:54 -0300

    update

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ export const WHATSAPP = '5491131332405';
 export const CLAVE_CARRITO = 'fc_carrito';
 
 // Supabase
-export const SUPABASE_URL  = 'https://lugqcahzmqnwrulynhkt.supabase.co';
-export const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1Z3FjYWh6bXFud3J1bHluaGt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMDg1NjMsImV4cCI6MjA5MDg4NDU2M30.lk9SYXVWm8X2caMctDSu22unfR6QKPZWYQBoLbKEEmc';
+export const SUPABASE_URL  = 'https://jxvecsbaphoqkhsekord.supabase.co';
+export const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4dmVjc2JhcGhvcWtoc2Vrb3JkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2ODU2MzMsImV4cCI6MjA5MTI2MTYzM30.nmDAwE3Kk9NTpnI8hK1qWZmqQJbcnrGTY4RDrtidCWI';
 // Paginación: productos por "página" antes del botón "Ver más"
 export const PRODUCTOS_POR_PAGINA = 24;
